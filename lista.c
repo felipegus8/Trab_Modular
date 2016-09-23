0375b597c7c49ede689904d48ef3a1a3b66afc27
VictorNogueiraRio <victor.nogueira@me.com>
2016-09-23 14:32:59 -0300

    Update lista.c

--- lista.c
+++ lista.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
+#include <assert.h>
+#include <string.h>
 #include "Lista.h"
+
+
+typedef struct tagElemLista {
+
+         void * pValor ;
+               /* Ponteiro para o valor contido no elemento */
+
+         struct tagElemLista * pAnt ;
+               /* Ponteiro para o elemento predecessor */
+
+         struct tagElemLista * pProx ;
+               /* Ponteiro para o elemento sucessor */
+
+   } tpElemLista ;
+
 
 typedef struct LIS_tagLista {
 
@@ -17,7 +34,7 @@ typedef struct LIS_tagLista {
          int numElem ;
                /* Número de elementos da lista */
                
-         char id[4];
+         char idLista[4];
               /*  String que identifica a lista */
          void ( * ExcluirValor ) ( void * pValor ) ;
                /* Ponteiro para a função de destruição do valor contido em um elemento */
@@ -40,13 +57,13 @@ typedef struct LIS_tagLista {
 *  Função: LIS  &Criar lista
 *  ****/
 
-   LIS_tppLista LIS_CriarLista(
-             void   ( * ExcluirValor ) ( void * pDado ), char *idLista )
+   LIS_tppLista LIS_CriarLista(char idLista[4]
+             void   ( * ExcluirValor ) ( void * pDado ))
    {
 
-      LIS_tpLista * pLista = NULL ;
+      LIS_tppLista * pLista = NULL ;
 
-      pLista = ( LIS_tpLista * ) malloc( sizeof( LIS_tpLista )) ;
+      pLista = ( LIS_tpLista * ) malloc( sizeof( LIS_tppLista )) ;
       if ( pLista == NULL )
       {
          return NULL ;
@@ -57,10 +74,62 @@ typedef struct LIS_tagLista {
       pLista->ExcluirValor = ExcluirValor;
       
       pLista->idLista = (char *) malloc(sizeof(char) * strlen(idLista));
+      
+      strcpy(pLista->idLista, idLista);
 
       return pLista ;
 
    } /* Fim função: LIS  &Criar lista */
+   
+   
+/***************************************************************************
+*
+*  Função: LIS  &Obter Id Lista
+*  ****/
+   
+   char *obterIdLista(LIS_tppLista lista) {
+            if(lista == NULL) {
+                     return NULL;
+            }
+             return lista->idLista;
+   } 
+
+/***************************************************************************
+*
+*  Função: LIS  &Inserir nó na lista
+*  ****/
+
+  int inserirNo(LIS_tppLista lista, char *elemento) {
+           tpElemLista *novo;
+           tpElemLista *aux;
+           #ifdef _DEBUG
+           assert( pLista != NULL ) ;
+           #endif
+           
+           novo =  (tpElemLista *) malloc(sizeof(tpElemLista));
+           if(novo == NULL) {
+                   // printf("falta de memória para inserir novo elemento na lista\n");
+                    return 6;
+           }
+           
+           tpElemLista->pValor = (char *) malloc(sizeof(char) * strlen(elemento));
+           if(tpElemLista->pValor) {
+                    // printf("falta de memória para inserir novo elemento na lista\n");
+                    return 6;
+           }
+           strcpy(tpElemLista->pValor, elemento);
+           aux = lista->pElemCorr->pProx;
+           novo->pAnt = lista->pElemCorr;
+           if(aux != NULL) {
+                    novo->pProx = aux;
+                    lista->pElemCorr->prox = novo;
+                    aux->ant = novo;
+           } else {
+                    novo->pProx = NULL;
+           }
+           return 0;
+  }
+
 
 /*
 char *obterIdLista(LIS_tppLista pLista) {
