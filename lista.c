5bec94ff1bc1359e9a238face7c0997495589e26
VictorNogueiraRio <victor.nogueira@me.com>
2016-09-22 23:06:35 -0300

    Update lista.c

--- lista.c
+++ lista.c
@@ -1,2 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+/*
+char *obterIdLista(LIS_tppLista pLista) {
+   #ifdef _DEBUG
+   assert(plista != NULL) 
+    #endif
+    if(pLista == NULL) {
+        return NULL;
+    }
+    return pLista->idLista;
+   }
+
+*/
