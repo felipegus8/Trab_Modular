07d1db25cf78130ac93b6560b2f652e0e5de41b6
VictorNogueiraRio <victor.nogueira@me.com>
2016-10-01 16:41:09 -0300

    Update Tabuleiro.c

---
+++ Tabuleiro.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <assert.h>
+#include <string.h>
+#include "Tabuleiro.h"
+
+
+typedef struct tagElemLista {
+
+         void * pValor ;
+               /* Ponteiro para o valor contido no elemento */
+
+         struct tagElemLista * pAnt ;
+               /* Ponteiro para o elemento predecessor */
+
+         struct tagElemLista * pProx ;
+               /* Ponteiro para o elemento sucessor */
+
+   } tpElemLista ;
+
+
+typedef struct LIS_tagLista {
+
+         tpElemLista * pOrigemLista ;
+               /* Ponteiro para a origem da lista */
+
+         tpElemLista * pFimLista ;
+               /* Ponteiro para o final da lista */
+
+         tpElemLista * pElemCorr ;
+               /* Ponteiro para o elemento corrente da lista */
+
+         int numElem ;
+               /* Número de elementos da lista */
+               
+         char idLista[4];
+              /*  String que identifica a lista */
+         void ( * ExcluirValor ) ( void * pValor ) ;
+               /* Ponteiro para a função de destruição do valor contido em um elemento */
+
+   } LIS_tppLista ;
+   
+   
+   
+   
+   
+   
+   
+   
+   
+
