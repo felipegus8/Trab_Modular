881e6060d563129754aad7c2c0403efb8e1b2bbe
VictorNogueiraRio <victor.nogueira@me.com>
2016-10-02 00:27:29 -0300

    Update Tabuleiro.c

--- Tabuleiro.c
+++ Tabuleiro.c
@@ -21,7 +21,10 @@
          /*ponteiro para a estrutura da peça contida na casa */
    } Casa;
     
-   Casa tabuleiro[8][8];  
+   Casa tabuleiro[8][8];
+   char idListaPeca[4] = "PeLI";
+   LIS_tppLista listaPecas;
+
 
    void destruirValor(void *pValor);
 
@@ -30,20 +33,17 @@
    
    }
    
-   TAB_tpCondRet criaTabuleiro(Casa **tabuleiro) {
+   TAB_tpCondRet criaTabuleiro() {
    
-    LIS_tppLista listaPecas;
     int i = 0,j=0;
     char a = 'A';
-    char b = 'B';
-    char idListaPeca[4] = "PeLI";
+    char b = 'B';    
     char idListaAmeacadosX = '1';//linha da casa onde reside a lista ameacados
     char idListaAmeacadosY; //coluna da casa onde reside a lista ameacados
     char idListaAmeacantesX = '1';//linha da casa onde reside a lista ameacantes
     char idListaAmeacadosY;//coluna da casa onde reside a lista ameacantes
     char idListaAmeacantes[4];
     char idListaAmecantes[4];
-    tabuleiro = malloc(sizeof(Casa) * 64);
     if(tabuleiro == NULL) {
          return TAB_CondRetFaltouMemoria;
     }        
@@ -91,6 +91,7 @@
        if(elemento == NULL) {
              return TAB_CondRetFaltouMemoria;
        }
+       elemento->id = (char *) malloc(sizeof(char));
        strcpy(elemento->id,id);
        elemento->x = movimentoX;
        elemento->y = movimentoY;
@@ -106,6 +107,7 @@
         if(novo == NULL) {
             return TAB_CondRetFaltouMemoria;
         }
+        
         strcpy(peca->cor,cor);
         strcpy(peca->id,id);
         tabuleiro[x][y]->peca = novo;
@@ -162,9 +164,49 @@
           peca = NULL;
    }
 
+   TAB_tpCondRet MoverPeca(int xo,char yo,int xd,char yd) {
+          char cor;
+          char id;
+          void *elemento;
+          Peca *peca;
+          LIS_tpCondRet ret;
+          obterpeca(x0,y0,&cor,&id);
+          obterno(lista,elemento);
+          peca = (Peca *)elemento;
+          while(strcmp(peca->id,id) != 0) {
+                if(obterno(lista,peca) == LIS_CondRetFimLista) {
+                     return TAB_CondRetNaoAchouPeca;
+                }
+                
+              irProx(listaPecas);
+          }
+          
+   }
   
+
+   void liberaPeca(Peca *peca) {
+         free(peca->cor);
+         free(peca->id);
+         free(peca);
+   }
+
+   TAB_tpCondRet DestruirTabuleiro() {
+          int i=0,j=0;
+          while(i<8) {
+               while(i<8) {
+                   if(tabuleiro[i][j] != NULL) {
+                     destroiLista(tabuleiro[i][j]->ameacados);
+                     destroiLista(tabuleiro[i][j]->ameacados);
+                     liberarPeca(tabuleiro[i][j]->peca);
+                   }
+               }
+          }
+   
+   }
 
    void destruirValor(void *pValor) {
         free(pValor);
    }
+
+
 
