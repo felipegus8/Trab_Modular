f849d9341d9131e77dbb279b66ad1d599e28bd8f
VictorNogueiraRio <victor.nogueira@me.com>
2016-10-04 18:18:40 -0300

    Update peca.c

--- peca.c
+++ peca.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "peca.h"
 
+
+
+typedef struct peca {
+        char *id;  // string identificadora da peça
+        char *cor; //cor da peça
+        int x; //quantidade de movimentos no eixo vertical
+        int y; // quantidade de movimentos no eixo horizontal
+   }Peca;  
