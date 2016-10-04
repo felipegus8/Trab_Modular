66c0d837d08e96dc7b5c34eb1911c6a6cbf0d0f3
VictorNogueiraRio <victor.nogueira@me.com>
2016-10-04 18:23:20 -0300

    Update peca.c

--- peca.c
+++ peca.c
@@ -10,3 +10,38 @@ typedef struct peca {
         int x; //quantidade de movimentos no eixo vertical
         int y; // quantidade de movimentos no eixo horizontal
    }Peca;  
+
+
+
+TAB_tpCondRet criaPeca(Peca *novo) {
+          novo = (Peca *) malloc(sizeof(Peca));
+          if(novo == NULL) {
+            return TAB_CondRetFaltouMemoria;
+          }
+          novo->id = (char *) sizeof(char);
+          if(novo->id == NULL) {
+            return TAB_CondRetFaltouMemoria;
+          }
+          *(novo->id) = 'V';
+          novo->cor = (char *) sizeof(char);
+          if(novo->cor == NULL) {
+            return TAB_CondRetFaltouMemoria;
+          }
+          *(novo->cor) = 'V';
+          novo.x = 0;
+          novo.y = 0;
+          return TAB_CondRetOK; 
+   }
+
+
+
+
+
+
+void liberaPeca(Peca *peca) {
+         free(peca->cor);
+         free(peca->id);
+         free(peca);
+   }
+
+
