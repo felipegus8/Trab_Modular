a03aaff209e4f73127aec7a53f5ef7e04f5f2966
felipegus8 <felipegus8@yahoo.com.br>
2016-10-04 20:04:41 -0300

    Ensina Movimentos

--- peca.c
+++ peca.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include "peca.h"
 
-
  struct peca {
         char *id;  // string identificadora da peça
         char *cor; //cor da peça
@@ -14,23 +13,44 @@ struct movimento{
     int y;
 };
 
-TAB_tpCondRet criaPeca(Peca *novo) {
+PEC_tpCondRet criaPeca(Peca *novo) {
           novo = (Peca *) malloc(sizeof(Peca));
           if(novo == NULL) {
-            return TAB_CondRetFaltouMemoria;
+            return PEC_CondRetFaltouMemoria;
           }
           novo->id = (char *) sizeof(char);
           if(novo->id == NULL) {
-            return TAB_CondRetFaltouMemoria;
+            return PEC_CondRetFaltouMemoria;
           }
           *(novo->id) = 'V';
           novo->cor = (char *) sizeof(char);
           if(novo->cor == NULL) {
-            return TAB_CondRetFaltouMemoria;
+            return PEC_CondRetFaltouMemoria;
           }
           *(novo->cor) = 'V';
-          return TAB_CondRetOK; 
+          return PEC_CondRetOK; 
    }
+
+PEC_tpCondRet ensinaMovimentosPecasconhecidas(Peca *novo)
+{
+	int i = 1,j = 1;
+	switch(novo->id)
+	{
+	case "T":
+		novo->movPeca = (Movimento*)malloc(sizeof(Movimento)*14);
+		for(i=0;i<8;i++)
+		{
+			novo->movPeca->x = i;
+			novo->movPeca->y = 0;
+		}
+		i = 1;
+		for(j=0;j<8;j++)
+		{
+			novo->movPeca->x = 0;
+			novo->movPeca->y = j;
+		}
+	}
+}
 
 
 
--- peca.h
+++ peca.h
@@ -2,5 +2,5 @@
 
 typedef struct peca Peca;
 typedef struct movimento Movimento;
-void criaPeca(Peca *novo);
+TAB_tpCondRet criaPeca(Peca *novo);
 void liberarPeca(Peca *peca);
