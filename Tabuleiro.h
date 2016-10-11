e2e374a18f14810ab6f68acb63782509e8e47868
felipegus8 <felipegus8@yahoo.com.br>
2016-10-11 01:25:01 -0300

    Erro meu deus do ceu

--- Tabuleiro.h
+++ Tabuleiro.h
@@ -119,7 +119,7 @@ TAB_tpCondRet TAB_CriaTabuleiro(ptTabuleiro tabuleiro);
 *     Se alguma das coordenadas passadas como parâmetro estiver fora do intervalo de tamanho do tabuleiro retorna CoordenadaNExiste.
 
 ***********************************************************************/
-TAB_tpCondRet TAB_insereTabuleiro insere(ptTabuleiro pTabuleiro,int x, int y,char cor,char id);
+TAB_tpCondRet TAB_insereTabuleiro(ptTabuleiro pTabuleiro,int x, int y,char cor,char id);
 
 
 
