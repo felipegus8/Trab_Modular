4c191ac687e5799d8929b77fc8b659871a540308
LuiisClaudio <luisclaudio97@hotmail.com>
2016-10-29 00:35:22 -0200

    primeior

---
+++ jogador.c
@@ -0,0 +1,23 @@
+/***************************************************************************
+ *  $MCI Módulo de implementação: Jogador
+ *  Arquivo gerado:              Jogador.c
+ *  Letras identificadoras:      //Completar
+ *
+ *  Autores: Felipe Viberti,Luis Claudio e Victor Nogueira
+ *
+ *  $HA Histórico de evolução:
+ *     Versão  Autores                                           Data          Observações
+ *     1       Felipe Viberti,Luis Claudio e Victor Nogueira   29/out/2016     início desenvolvimento
+ *
+ ***************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include "Tabuleiro.h"
+
+typedef stuct jogador
+{
+	char nome[30];
+	char cor;
+}
