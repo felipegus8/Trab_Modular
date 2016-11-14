36d669a61d87ffcbafb1b5f1b0b70d6ad2d2d460
LuiisClaudio <luisclaudio97@hotmail.com>
2016-11-14 13:17:51 -0200

    Só Update mesmo do TesteJogo.c

--- TesteJogo.c
+++ TesteJogo.c
@@ -25,18 +25,35 @@
 
 /*
 JOG_tpCondRet JOG_EfetuarJogada(ptJudge j, char corDaVez,int posIniX,int posIniY,int posFimX,int posFimY);
-
-
-
 JOG_tpCondRet JOG_CriaJuiz(ptJudge *j,ptJogador a,ptJogador b);
-
 JOG_tpCondRet JOG_ComecarJogo(ptJudge j,ptJogador a,ptJogador b,char *nomeA,char *nomeB,int (*InserirPecas)(TAB_tpCondRet(*InserirNoTab)(ptTabuleiro,int x,int y,char cor,char id)));
 JOG_tpCondRet JOG_AssasinarJuiz(ptJudge j);
+
+JOG_tpCondRet JOG_ObtemTabuleiro(ptJudge j,ptTabuleiro *tabu);
+
+JOG_tpCondRet JOG_GeraMatrizTabuleiro(ptJudge j,char matriz[8][8][2]);
+
+JOG_tpCondRet JOG_CriaJogador(ptJogador *j,char *nome,char cor);
+
+JOG_tpCondRet JOG_RetornoNomeJogador1(ptJudge j,char nome[30]);
+
+JOG_tpCondRet JOG_RetornoNomeJogador2(ptJudge j,char nome[30]);
+
+JOG_tpCondRet JOG_DevolveAmeacantes(ptJudge j,char *corAmeacantes,char *idAmeacantes,int *qtdAmeacantes,int x,int y);
+
+JOG_tpCondRet JOG_DevolveAmeacados(ptJudge j,char *corAmeacados,char *idAmeacados,int *qtdAmeacados,int x,int y);
 */
 static const char EFETUAR_JOGADA             							[ ] = "=efetuarjogada" ;
 static const char CRIA_JUIZ          	                                [ ] = "=criajuiz"   ;
 static const char COMECAR_JOGO              	                        [ ] = "=comecarjogo"      ;
-static const char ASSASINAR_JUIZ               							[ ] = "=assasinarjuiz"        ;
+static const char ASSASINAR_JUIZ                						[ ] = "=assasinarjuiz"        ;
+static const char OBTEM_TABULEIRO               						[ ] = "=obtemtabuleiro"        ;
+static const char GERA_MATRIZ_TABULEIRO              					[ ] = "=geramatriztabuleiro"        ;
+static const char CRIA_JOGADOR              							[ ] = "=criajogador"        ;
+static const char RETORNO_NOME_JOGADOR_1               					[ ] = "=retornonomejogador1"        ;
+static const char RETORNO_NOME_JOGADOR_2               					[ ] = "=retornonomejogador2"        ;
+static const char DEVOLVE_AMEACANTES              						[ ] = "=devolveameacantes"        ;
+static const char DEVOLVE_AMEACADOS               						[ ] = "=devolveameacados"        ;
 
 #define TRUE  1
 #define FALSE 0
@@ -47,6 +64,7 @@ static const char ASSASINAR_JUIZ               							[ ] = "=assasinarjuiz"    
 
 ptJudge juiz;
 ptJogador JogadorA, JogadorB;
+ptTabuleiro tabu;
 //#define DIM_VT_PECA   100
 //#define DIM_VALOR     100
 /***********************************************************************
@@ -69,7 +87,7 @@ TST_tpCondRet TST_EfetuarComando( char * ComandoTeste )
 {
 	 int numLidos   = -1 , CondRetEsp = -1  ;
 	 TST_tpCondRet CondRet ;
-     char   StringDado[  DIM_VALOR ],StringDado2[DIM_VALOR] ;
+     char   StringDado[  DIM_VALOR ],StringDado2[DIM_VALOR], id ;
      int ValEsp = -1 ;
      int i ;
      int numElem = -1 ;
@@ -86,9 +104,9 @@ TST_tpCondRet TST_EfetuarComando( char * ComandoTeste )
 		   CondRet = JOG_CriaJuiz(&juiz, JogadorA, JogadorB);
 		   printf("%d\n",CondRet);
 
-            return TST_CompararPonteiroNulo( 1 , juiz , "Erro em criar juiz"  ) ;
-
+			return TST_CompararInt( CondRetEsp , CondRet ,"Erro em criar juiz" );
        }/*Fim ativa: Efetuar Jogada */
+
 	   //Inicio da Efetuar jogada
 	   else if ( strcmp( ComandoTeste , EFETUAR_JOGADA) == 0 )
          {
@@ -99,7 +117,8 @@ TST_tpCondRet TST_EfetuarComando( char * ComandoTeste )
             {
                return TST_CondRetParm ;
             } /* if */
-
+			CondRet = JOG_EfetuarJogada(juiz, corDaVez, xi, yi, xf, yf, CondRetEsp);
+			//JOG_tpCondRet JOG_EfetuarJogada(ptJudge j, char corDaVez,int posIniX,int posIniY,int posFimX,int posFimY);
             return TST_CompararInt( CondRetEsp , CondRet ,"Erro ao efetuar Jogada" );
          }//Fim da efetuar jogada
 	   //Inicio da assasinar juiz
@@ -113,20 +132,53 @@ TST_tpCondRet TST_EfetuarComando( char * ComandoTeste )
                return TST_CondRetParm ;
             } /* if */
 
-             return TST_CompararPonteiroNulo( 1 , juiz , "Erro em assasinar juiz"  ) ;
+			CondRet = JOG_AssasinarJuiz(juiz);
+			//JOG_tpCondRet JOG_AssasinarJuiz(ptJudge j);
+             return TST_CompararInt( CondRetEsp , CondRet ,"Assasinar Juiz" );
          }//Fim de assasinar Juiz
 	   //Inicio de começar Jogo
 		else if ( strcmp( ComandoTeste , COMECAR_JOGO) == 0 )
          {
 
-            numLidos = LER_LerParametros( "iiissii" ,&juiz, JogadorA, JogadorB, StringDado, StringDado2, &CondRetEsp, &CondRetEsp); 
+            numLidos = LER_LerParametros( "ssi" ,StringDado, StringDado2, &CondRetEsp); 
 
-            if (  numLidos != 7 )
+            if (  numLidos != 3 )
             {
                return TST_CondRetParm ;
             } /* if */
 
+			CondRet = JOG_tpCondRet JOG_ComecarJogo(juiz, TAB_InserirPeca(xi, yi, corDaVez, id)
+			//JOG_tpCondRet JOG_ComecarJogo(ptJudge j,JOG_tpCondRet (*InserirPecas)(TAB_tpCondRet(*InserirNoTab)(ptTabuleiro,int x,int y,char cor,char id),ptJudge j));
             return TST_CompararInt( CondRetEsp , CondRet ,"Erro ao ComeçarJogo" );
          }//Fim de começar jogo
+		   //Inicio obtem tabuleiro 
+		else if ( strcmp( ComandoTeste , OBTEM_TABULEIRO) == 0 )
+         {
 
+            numLidos = LER_LerParametros( "i", &CondRetEsp); 
+
+            if (  numLidos != 1 )
+            {
+               return TST_CondRetParm ;
+            } /* if */
+
+			//JOG_tpCondRet JOG_ObtemTabuleiro(ptJudge j,ptTabuleiro *tabu);
+            return TST_CompararInt( CondRetEsp , CondRet ,"Erro em obter tabuleiro" );
+         }
+		//Fim de obtem tabuleiro
 }
+/*
+JOG_tpCondRet JOG_ObtemTabuleiro(ptJudge j,ptTabuleiro *tabu);
+
+JOG_tpCondRet JOG_GeraMatrizTabuleiro(ptJudge j,char matriz[8][8][2]);
+
+JOG_tpCondRet JOG_CriaJogador(ptJogador *j,char *nome,char cor);
+
+JOG_tpCondRet JOG_RetornoNomeJogador1(ptJudge j,char nome[30]);
+
+JOG_tpCondRet JOG_RetornoNomeJogador2(ptJudge j,char nome[30]);
+
+JOG_tpCondRet JOG_DevolveAmeacantes(ptJudge j,char *corAmeacantes,char *idAmeacantes,int *qtdAmeacantes,int x,int y);
+
+JOG_tpCondRet JOG_DevolveAmeacados(ptJudge j,char *corAmeacados,char *idAmeacados,int *qtdAmeacados,int x,int y);
+*/
